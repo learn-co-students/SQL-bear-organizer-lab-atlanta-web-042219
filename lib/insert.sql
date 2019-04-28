0ef7e7f1f8e9daed3845b5e053c0c6ffe90797cf
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-04-28 18:11:16 -0400

    Basic SQL Done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+('Mr.Chocolate', 3, 'Male', 'Brown', 'Wild', 1),
+('Rowdy', 6, 'Male', 'Black', 'Tame', 1),
+('Tabitha', 10, 'Female', 'White', 'Melow', 0),
+('Sergeant Brown', 7, 'Male', 'Black', 'Wild', 1),
+('Melissa', 9, 'Female', 'Brown', 'Tame', 1),
+('Grinch', 5, 'Male', 'Brown', 'Wild', 0),
+('Wendy', 2, 'Female', 'White', 'Sweet', 1),
+(NULL, 4, 'Female', 'Black', 'Wild', 1);
