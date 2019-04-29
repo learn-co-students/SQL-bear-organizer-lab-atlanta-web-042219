10bf426180b88bccd96ab0b0f92ec373a54851a8
ascotttoney <ascotttoney@gmail.com>
2019-04-29 09:56:16 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+INSERT INTO bears (
+    name,
+    age,
+    gender,
+    color,
+    temperament,
+    alive
+)
+
+VALUES (
+    "Mr Chocolate",
+    5,
+    "male",
+    "brown",
+    "calm",
+    true
+    ), (
+    "Rowdy",
+    6,
+    "male",
+    "brown",
+    "crazy",
+    true
+    ), (
+    "Tabitha",
+    5,
+    "female",
+    "brown",
+    "calm",
+    true
+    ), (
+    "Sergeant Brown",
+    5,
+    "male",
+    "brown",
+    "happy",
+    true
+    ), (
+    "Melissa",
+    8,
+    "female",
+    "black",
+    "calm",
+    true
+    ), (
+    "Grinch",
+    5,
+    "male",
+    "brown",
+    "grumpy",
+    true
+    ), (
+    "Wendy",
+    2,
+    "female",
+    "brown",
+    "silly",
+    true
+    ), (
+    NULL,
+    9,
+    "male",
+    "black",
+    "crazy",
+    true);
