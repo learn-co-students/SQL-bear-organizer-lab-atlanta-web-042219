afb03326f10cece70eca10672df6da6d8ce2d40f
seerosecode <keshalakemusic@gmail.com>
2019-04-29 14:44:54 -0400

    almost

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 5, "male", "brown", "hungry", TRUE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 1, "male", "black", "hungry", TRUE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 10, "female", "light brown", "calm", TRUE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 1, "male", "dark brown", "playful", TRUE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 5, "female", "light brown", "lazy", FALSE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 9, "male", "white", "aggressive", TRUE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 3, "female", "black", "sweet", TRUE);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES (null, 3, "female", "brown", "aggressive", FALSE);
